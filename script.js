b0c8f251cf9c47be8be4d5db70477ce54170470f
AmauriBlanco <blancoamauri@gmail.com>
2023-11-29 18:03:14 -0300

    4-2 Declarando e nomeando variáveis

--- script.js
+++ script.js
@@ -7,3 +7,18 @@
     ----------  ----------  ---------- 
     idade         nome        cor
 */
+
+/**
+ * 1 - Inicio -> letras, cifrão($) e underscore(_)
+ * 2 - conter -> letras, cifrão($) e underscore(_)
+ * 3 - Palavras reservadas.(for, if, while, typeof...)
+ * 4 - Case sensitive
+ * 5 - Camal Case
+ */
+
+let corDoCeu = "Azul";
+let nomeDoUsuario = "Amauri";
+let idade = 33;
+let casado = true;
+
+console.log(corDoCeu);
