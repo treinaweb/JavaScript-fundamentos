0ecc92f422658a2bade50b38da62f16506093b2d
AmauriBlanco <blancoamauri@gmail.com>
2023-11-30 22:02:44 -0300

    7-2 Laços de Repetição com for

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-let mes = 13,
-    nomeMes = "";
-
-switch(mes) {
-    case 1: nomeMes = "Janeiro"; break
-    case 2: nomeMes = "Fevereiro"; break
-    case 3: nomeMes = "Março"; break
-    case 4: nomeMes = "Abril"; break
-    case 5: nomeMes = "Maio"; break
-    case 6: nomeMes = "Junho"; break
-    case 7: nomeMes = "Julho"; break
-    case 8: nomeMes = "Agosto"; break
-    case 9: nomeMes = "Setembro"; break
-    case 10: nomeMes = "Outubro"; break
-    case 11: nomeMes = "Novembro"; break
-    case 12: nomeMes = "Dezembro"; break
-    default: nomeMes = "Mês inválido";
+for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} x ${j} = ${i * j}`);
+    }
 }
-
-console.log(nomeMes);
