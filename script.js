dd3558a3ca1839fea976ca5d34e08ff982538780
AmauriBlanco <blancoamauri@gmail.com>
2023-11-30 21:09:15 -0300

    Comparando vários valores com Switch

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-let hora = 11,
-    mensagem = "";
+let mes = 13,
+    nomeMes = "";
 
-if (hora < 12) {
-    mensagem = "Bom dia";
-} else if (hora >= 12 && hora < 18) {
-    mensagem = "Boa tarde";
-} else {
-    mensagem = "Boa noite";
+switch(mes) {
+    case 1: nomeMes = "Janeiro"; break
+    case 2: nomeMes = "Fevereiro"; break
+    case 3: nomeMes = "Março"; break
+    case 4: nomeMes = "Abril"; break
+    case 5: nomeMes = "Maio"; break
+    case 6: nomeMes = "Junho"; break
+    case 7: nomeMes = "Julho"; break
+    case 8: nomeMes = "Agosto"; break
+    case 9: nomeMes = "Setembro"; break
+    case 10: nomeMes = "Outubro"; break
+    case 11: nomeMes = "Novembro"; break
+    case 12: nomeMes = "Dezembro"; break
+    default: nomeMes = "Mês inválido";
 }
 
-console.log(mensagem);
+console.log(nomeMes);
